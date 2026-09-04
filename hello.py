3c6c20dcb5559b97ddb329ecdb88cdbd56e66f25
Haw Zong Han <2601880@sit.singaporetech.edu.sg>
2026-09-04 15:45:29 +0800

    Followers

--- hello.py
+++ hello.py
@@ -11,3 +11,11 @@ print("Username: ", username)
 print("Bio", bio)
 print("Followers", followers)
 
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers -= 10
+print("Day 3:", followers)

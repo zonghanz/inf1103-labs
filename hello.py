92308ad3695f1303e3ee3f4b7c9578033a6966e9
Haw Zong Han <2601880@sit.singaporetech.edu.sg>
2026-09-04 15:44:58 +0800

    Personal information

--- hello.py
+++ hello.py
@@ -3,32 +3,11 @@ print("Welcome here")
 print("My first post!")
 print("=====")
 
-# username = "xmaster_dumbbells"
-# bio = "runner"
-# followers = 100
+username = "xmaster_dumbbells"
+bio = "runner"
+followers = 100
 
-# print("Username: ", username)
-# print("Bio", bio)
-# print("Followers", followers)
+print("Username: ", username)
+print("Bio", bio)
+print("Followers", followers)
 
-# followers += 50
-# print("Day 1:", followers)
-
-# followers += 20
-# print("Day 2:", followers)
-
-# followers -= 10
-# print("Day 3:", followers)
-
-# username = input("Enter Username: ")
-# age = int(input("Enter Age: "))
-# category = input("Enter Content Category: ")
-
-# print("\nInstagram Profile")
-# print("=========")
-# print("Username: ", username)
-# print("Age: ", age)
-# print("Category: ", category)
-
-# if age>40 and category == "fun":
-#     print("You are old what is fun for you??")

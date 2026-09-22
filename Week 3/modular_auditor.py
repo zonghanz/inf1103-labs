cab22227fce04854c0a816cdf693b7727d1e005c
Haw Zong Han <2601880@sit.singaporetech.edu.sg>
2026-09-22 17:39:33 +0800

    Lab 3 Requirement 3

--- Week 3/modular_auditor.py
+++ Week 3/modular_auditor.py
@@ -1,34 +1,49 @@
 inventory = 0
 loop = True
 failed = 0
-total_units = 0
 
+# ===== Functions =====
 def get_valid_input():
     stock_quantity = input("Enter stock quantity. Enter 'quit' to exit:")
     if (stock_quantity == "quit"):
         return stock_quantity
     
-    elif ((stock_quantity.isdigit() == False) & (stock_quantity != 0)):
-        print("Enter a positive integer that is not 0")
+    elif (stock_quantity.isdigit() == False):
+        print("Enter a positive integer")
         return None
     
-    elif (int(stock_quantity) < 0):
-        print("Do not enter negative numbers")
+    elif (int(stock_quantity) == 0):
+        print("Enter a value greater than 0")
         return None
     
     else:
-        return stock_quantity
+        return int(stock_quantity)
 
+def process_delivery(current_total, new_value):
+    new_total = current_total + new_value
+    return new_total
+
+def calculate_tax(amount):
+    tax = amount * 0.1
+    return tax
+
+
+
+    
+
+# ===== Main Program =====
 while loop:
     stock_quantity= get_valid_input()
     if (stock_quantity == "quit"):
-        print("Total units processed: ", total_units, "\nTotal number of failed/rejected entries: ", failed)
+        print("Total units processed: ", inventory, "\nTotal number of failed/rejected entries: ", failed)
         break
+
     elif (stock_quantity == None):
         failed += 1
-    else:
-        inventory += int(stock_quantity)
-        total_units += 1
+
+    else: #user's value is valid
+        inventory = process_delivery(inventory, stock_quantity)
+
         if inventory > 500:
             print("Alert! Overstock!")
             break
